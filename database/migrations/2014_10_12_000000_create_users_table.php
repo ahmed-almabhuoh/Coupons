355434235bfd289edaf7a3b47b80c58608c54a33
Ahmad Almabhouh <az54546@gmail.com>
2023-04-04 11:10:08 +0300

    Bug

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,8 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fname');
+            $table->string('lname');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->boolean('is_active')->default(false);
+            $table->string('image')->nullable();
+            $table->enum('status', ['active', 'disabled']);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
