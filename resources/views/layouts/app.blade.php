06fd0145da237e25253fc26c7edbb9cafd722d2e
Ahmad Almabhouh <az54546@gmail.com>
2023-04-11 23:31:39 +0300

    Fix Bugs

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -332,11 +332,13 @@
                 <li class="nav-item dropdown dropdown-user"><a class="nav-link dropdown-toggle dropdown-user-link"
                         id="dropdown-user" href="#" data-bs-toggle="dropdown" aria-haspopup="true"
                         aria-expanded="false">
-                        <div class="user-nav d-sm-flex d-none"><span class="user-name fw-bolder">John Doe</span><span
-                                class="user-status">Admin</span></div><span class="avatar"><img class="round"
-                                src="{{ asset('panel/app-assets/images/portrait/small/avatar-s-11.jpg') }}"
-                                alt="avatar" height="40" width="40"><span
-                                class="avatar-status-online"></span></span>
+                        <div class="user-nav d-sm-flex d-none"><span
+                                class="user-name fw-bolder">{{ auth()->user()->fname . ' ' . auth()->user()->lname }}</span><span
+                                class="user-status">Admin</span></div><span class="avatar">
+                            <img class="round" src="{{ env('APP_URL') . 'human/' . auth()->user()->image }}"
+                                alt="avatar" height="40" width="40">
+                            <span class="avatar-status-online"></span>
+                        </span>
                     </a>
                     <div class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdown-user"><a
                             class="dropdown-item" href="page-profile.html"><i class="me-50"
@@ -522,6 +524,7 @@
         <div class="shadow-bottom"></div>
         <div class="main-menu-content">
             <ul class="navigation navigation-main" id="main-menu-navigation" data-menu="menu-navigation">
+                {{-- Ahmad Here --}}
                 @include('../partials/nav')
             </ul>
         </div>
