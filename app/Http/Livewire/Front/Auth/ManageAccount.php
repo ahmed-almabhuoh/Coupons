be6b85c15e6f844c48738b4ad75d55bd737f9af6
Ahmad Almabhouh <az54546@gmail.com>
2023-04-26 15:52:45 +0300

    Client Account Management

--- app/Http/Livewire/Front/Auth/ManageAccount.php
+++ app/Http/Livewire/Front/Auth/ManageAccount.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Livewire\Front\Auth;
 
+use App\Models\User;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class ManageAccount extends Component
 {
+    use WithFileUploads;
+
     public $fullname;
     public $email;
     public $phone;
@@ -17,12 +21,43 @@ class ManageAccount extends Component
         $user = auth()->user();
         $this->fullname = $user->fname . ' ' . $user->lname;
         $this->email = $user->email;
-        $this->phone = $user->phone;
+        if (!is_null($user->phone)) {
+            $this->phone = $user->phone;
+        } else {
+            $this->phone = '966 |';
+        }
         $this->notification = $user->notification;
     }
 
     public function render()
     {
         return view('livewire.front.auth.manage-account');
+    }
+
+    public function updateAccount()
+    {
+        $data = $this->validate([
+            'fullname' => 'required|string|min:2|max:25',
+            'email' => 'required|email|unique:users,email,' . auth('client')->user()->id,
+            'phone' => 'nullable|unique:users,phone,' . auth('client')->user()->id,
+            'image' => 'nullable',
+            'notification' => 'required|boolean',
+        ]);
+
+        $user = User::where('id', auth('client')->user()->id)->first();
+        $names = explode(" ", $this->fullname);
+        $user->fname = $names[0];
+        $user->lname = $names[1];
+        $user->email = $data['email'];
+        $user->phone = $data['phone'];
+        $user->notification = $data['notification'];
+        if ($data['image']) {
+            $path = $data['image']->store('users', [
+                'disk' => 'human_resources',
+            ]);
+            $user->image = $path;
+        }
+
+        $user->save();
     }
 }
--- resources/views/livewire/front/auth/manage-account.blade.php
+++ resources/views/livewire/front/auth/manage-account.blade.php
@@ -1,9 +1,10 @@
 <div class="card-wrapper">
     <!-- Add Image -->
     <div>
-        <label for="imageUpload" class="custom-file-upload">
+        <label for="imageUpload" class="custom-file-upload" style="background-image: ;">
+
         </label>
-        <input type="file" wire:model="image" id="imageUpload">
+        <input type="file" id="imageUpload" wire:model="image">
         <div id="imagePreview"></div>
     </div>
     <!--Start Card -->
@@ -14,25 +15,45 @@
                     <label for="fullname"> {{ __('Full Name') }} </label>
                     <input type="text" class="first form-control" wire:model="fullname" name="name" required
                         autofocus>
+                    @error('fullname')
+                        <small style="color: red;">
+                            {{ $message }}
+                        </small>
+                    @enderror
                 </div>
             </div>
             <div class="form-group">
                 <label for="email"> {{ __('E-Mail Address') }} </label>
                 <input id="email" type="email" wire:model="email" class="form-control" name="email" required>
+                @error('email')
+                    <small style="color: red;">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <div class="form-group">
-                <label for="Phone"> {{ __('Phone Number') }} </label>
-                <input id="Phone" type="tel" class="form-control" wire:model="phone" name="Phone" required
+                <label for="phone"> {{ __('Phone Number') }} </label>
+                <input id="phone" type="tel" class="form-control" wire:model="phone" name="phone" required
                     value="966 |">
+                @error('phone')
+                    <small style="color: red;">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <!-- Switch -->
             <div class="form-check form-switch">
                 <label class="form-check-label" for="flexSwitchCheckDefault"> {{ __('Notification') }} </label>
                 <input class="form-check-input" type="checkbox" wire:model="notification" role="switch"
                     id="flexSwitchCheckDefault">
+                @error('notification')
+                    <small style="color: red;">
+                        {{ $message }}
+                    </small>
+                @enderror
             </div>
             <div class="form-group no-margin">
-                <button type="submit" class="btn btn-primary btn-block">
+                <button type="button" wire:click="updateAccount()" class="btn btn-primary btn-block">
                     {{ __('Save Edit') }}
                 </button>
             </div>
