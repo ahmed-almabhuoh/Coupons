232e35054660ea00ea0257b4736448745a586f08
Ahmad Almabhouh <az54546@gmail.com>
2023-05-14 19:16:56 +0300

    Fix Cancel Button

--- resources/views/components/form/submit.blade.php
+++ resources/views/components/form/submit.blade.php
@@ -1,5 +1,6 @@
 <div class="col-sm-6 col-12  py-1">
     <button class="btn btn-primary waves-effect waves-float waves-light" type="{{ $type }}"
         wire:click="{{ $action }}">{{ __($text) }}</button>
-    <button class="btn btn-secondary waves-effect waves-float waves-light" type="reset">{{ __('Cancel') }}</button>
+    <a class="btn btn-secondary waves-effect waves-float waves-light"
+        href="{{ url()->previous() }}">{{ __('Cancel') }}</a>
 </div>
