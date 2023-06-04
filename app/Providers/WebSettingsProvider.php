c3f219d94507aaa38e0c1bae293bb3fa35fbf737
Ahmad Almabhouh <az54546@gmail.com>
2023-06-04 10:20:56 +0300

    Fix Mobile Swipper Bugs

--- app/Providers/WebSettingsProvider.php
+++ app/Providers/WebSettingsProvider.php
@@ -7,9 +7,11 @@ use App\Models\Coupon;
 use App\Models\Offer;
 use App\Models\Product;
 use App\Models\Store;
+use Illuminate\Routing\Route;
 use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Route as FacadesRoute;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\View;
@@ -35,17 +37,20 @@ class WebSettingsProvider extends ServiceProvider
         $website_settings = DB::table('website_settings')->first();
 
         // dd(Cookie::get('new_selected_country'), 'Here');
+        // dd();
+        $currentURL = url()->current() . '/';
 
         // Fetch Countries
         $countries = Country::active()->get();
         // dd($countries);
 
         // Share the value with all views
-        View::composer('*', function ($view) use ($website_settings, $countries) {
+        View::composer('*', function ($view) use ($website_settings, $countries, $currentURL) {
             // $view->with('new_website_settings', $website_settings);
             $view->with([
                 'new_website_settings' => $website_settings,
                 'countries' => $countries,
+                'currentURL' => $currentURL,
             ]);
         });
     }
--- resources/views/frontend/client-v1/partials/mobile-swiper.blade.php
+++ resources/views/frontend/client-v1/partials/mobile-swiper.blade.php
@@ -1,9 +1,9 @@
 <!-- Start new button -->
 <div class="show-btn pt-2">
     <div class=" new-btn d-flex justify-content-center ">
-        <a class="@if (Route::currentRouteName() == 'pages.home') active @endif btn rounded-pill main-btn"
+        <a class="@if ($currentURL == env('APP_URL') . '' || $currentURL == env('APP_URL') . 'livewire/message/front.client.home/') active @endif btn rounded-pill main-btn"
             href="{{ route('pages.home') }}" onclick="">{{ __('Coupons') }}</a>
-        <a class="@if (Route::currentRouteName() == 'pages.offers') active @endif  btn rounded-pill main-btn"
+        <a class="@if ($currentURL == env('APP_URL') . 'offers/' || $currentURL == env('APP_URL') . 'livewire/message/front.client.offers/') active @endif  btn rounded-pill main-btn"
             href="{{ route('pages.offers') }}" onclick=""> {{ __('Offers') }} </a>
     </div>
 </div>
